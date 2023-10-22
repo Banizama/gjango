de5f1da8a9115c3c3272ab75ceb6ab1793022b75
Banizama <ovsiknazik@gmail.com>
2023-10-22 20:00:00 +0300

    made db

--- Negr/Negr_app/models.py
+++ Negr/Negr_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='static/images')
+    created_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+
+
+
