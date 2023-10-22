414f34563e1d316320eac5556d613efac273ae71
Banizama <ovsiknazik@gmail.com>
2023-10-22 18:25:24 +0300

    start project

---
+++ Negr/Negr_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NegrAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Negr_app'
---
+++ Negr/Negr_app/urls.py
@@ -0,0 +1,6 @@
+from my_app import views
+from other_app.views import Home
+
+utlpatterns = [
+
+]
