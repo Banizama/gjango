15292205a5d223f51f2dcda4634d654e9a6bb50b
Banizama <ovsiknazik@gmail.com>
2023-10-28 22:04:34 +0300

    add users table

--- Negr/Negr_app/models.py
+++ Negr/Negr_app/models.py
@@ -13,5 +13,12 @@ class Products(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
 
+class Users(models.Model):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    age = models.IntegerField()
+
 
 
