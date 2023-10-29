224c678e164329690f6decea694c079868c6cf7f
Banizama <ovsiknazik@gmail.com>
2023-10-29 20:02:27 +0200

    registered more models

--- Negr/Negr_app/admin.py
+++ Negr/Negr_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Products, Singers, Songs
 # Register your models here.
+
+admin.site.register(Products)
+admin.site.register(Singers)
+admin.site.register(Songs)
