e258a5ab0a8f0eb558cf4cca0294d9535ce51e29
Banizama <ovsiknazik@gmail.com>
2023-10-29 17:57:05 +0200

    made tables for songs and singers

--- Negr/Negr_app/models.py
+++ Negr/Negr_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Country(models.Model):
     name = models.CharField(max_length=100)
 
@@ -8,8 +9,8 @@ class Products(models.Model):
     name = models.CharField(max_length=50)
     price = models.FloatField()
     image = models.ImageField(upload_to='static/images')
-    created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
 
@@ -17,13 +18,13 @@ class Users(models.Model):
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateTimeField()
     age = models.IntegerField()
 
 
 class Singers(models.Model):
     name = models.CharField(max_length=30)
-    year = models.DateField()
+    year = models.DateTimeField()
 
 
 class Songs(models.Model):
