db9619d93d464571c3b50a55cfdc78444421a270
Banizama <ovsiknazik@gmail.com>
2023-10-30 14:53:23 +0200

    fixed models name

--- Negr/Negr_app/admin.py
+++ Negr/Negr_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Products, Singers, Songs
+from .models import Products, Artist, Albums
 # Register your models here.
 
 admin.site.register(Products)
-admin.site.register(Singers)
-admin.site.register(Songs)
+admin.site.register(Artist)
+admin.site.register(Albums)
