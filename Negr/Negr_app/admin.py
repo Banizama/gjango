1b91579c182499bd9c4a85ff552169d58e6fa6b9
Banizama <ovsiknazik@gmail.com>
2023-11-11 15:31:50 +0200

    add tables to admin.py

--- Negr/Negr_app/admin.py
+++ Negr/Negr_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Products, Artist, Albums, Instruments, Musician, Directors, Films
+from .models import Products, Artist, Albums, Instruments, Musician, Directors, Films, Cart, Country, Admin
 # Register your models here.
 
 admin.site.register(Products)
@@ -9,3 +9,6 @@ admin.site.register(Instruments)
 admin.site.register(Musician)
 admin.site.register(Directors)
 admin.site.register(Films)
+admin.site.register(Cart)
+admin.site.register(Country)
+admin.site.register(Admin)
