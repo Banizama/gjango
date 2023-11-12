53c6d14342a52561a5e22eea16f3687815baf018
Banizama <ovsiknazik@gmail.com>
2023-11-12 17:15:39 +0200

    add book table

--- Negr/Negr_app/models.py
+++ Negr/Negr_app/models.py
@@ -74,4 +74,9 @@ class Admin(models.Model):
     role = models.CharField(default='admin', max_length=5)
 
 
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
+    year = models.DateField()
+    author = models.CharField(max_length=100)
 
