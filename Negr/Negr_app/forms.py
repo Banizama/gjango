b75e1667ac84358140057412e9d495a85010c909
Banizama <ovsiknazik@gmail.com>
2023-11-18 15:02:18 +0200

    h/w 18.11.23

--- Negr/Negr_app/forms.py
+++ Negr/Negr_app/forms.py
@@ -4,16 +4,30 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 
-class RegistrationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
+# class RegistrationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+#         help_texts = {'username': '', 'email': '', 'password': ''}
 
+
+class RegistrationForm(UserCreationForm):
+    # def __init__(self, *args, **kwargs):
+    #     super(UserCreationForm, self).__init__(*args, **kwargs)
+    #     self.fields['username'].help_text = ''
+    #     self.fields['password1'].help_text = ''
+    #     self.fields['password2'].help_text = ''
+    username = forms.CharField(help_text="")
+    email = forms.EmailField(help_text="")
+    password1 = forms.CharField(help_text="", widget=forms.PasswordInput)
+    password2 = forms.CharField(help_text="", widget=forms.PasswordInput)
 
 class LoginForm(AuthenticationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+    # class Meta:
+    #     model = User
+    #     fields = ['username', 'password']
+    username = forms.CharField(help_text="")
+    password = forms.CharField(help_text="", widget=forms.PasswordInput)
 
 
 class FilmForm(forms.Form):
